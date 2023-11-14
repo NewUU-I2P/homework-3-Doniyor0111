af29d6bb86b7aa95277afde6cb09e5f69d188b4d
Doniyor <104784498+Doniyor0111@users.noreply.github.com>
2023-11-14 22:22:03 +0500

    problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,15 +1,34 @@
 void problemSolution1(float consumed_water) {
     float cost;
     // write your code here
-    cost = 13;
-    if (consumed_water <= 30) {
-        cost += consumed_water * 0.4;
-    } else if (consumed_water <= 50) {
-        cost += 30 * 0.4 + (consumed_water - 30) * 0.12;
-    } else if (consumed_water <= 60) {
-        cost += 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4;
-    } else {
-        cost += 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water - 60) * 1.5;
+    #include <iostream>
+
+using namespace std;
+int main () {
+    double fixamount = 13;
+    double first30 = 0.4;
+    double sec20 = 0.12;
+    double thr10 = 1.4;
+    double add = 1.5;
+    int cost;
+    cout<<"Consuption = "<<endl;
+    std::cin>>cost;
+
+
+    if (cost <= 30){
+       cout<< double (fixamount + first30 * cost) << endl;
     }
-    return cost;
+    else if (cost <= 50){
+        cout << double ( fixamount + first30 * 30 + (cost - 30) * sec20) << endl;
+    }
+    else if (cost <= 60){
+        cout << double ( fixamount + first30 * 30 + sec20 * 20 + (cost - 50) * thr10) << endl;
+    }
+    else {
+        cout << double ( fixamount + first30 * 30 + sec20 * 20 + thr10 * 10 + (cost - 60) * add) << endl;
+    }
+
+
+    return 0;
 }
+
