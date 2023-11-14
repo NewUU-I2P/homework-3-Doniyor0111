4a5ca479ecf1c29f897552d5b83fc29ba2c2be35
Doniyor <104784498+Doniyor0111@users.noreply.github.com>
2023-11-14 22:44:41 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -6,13 +6,13 @@ std::string problemSolution3(float height, char S) {
     // write your code here
     if (S == 'M' && height < 1.7){
     }
-    else if (S == 'M' && height >= 1.7 && hight < 1.85){
+    else if (S == 'M' && height >= 1.7 && height < 1.85){
     }
     else if (S == 'M' && height >= 1.85){
     }
     else if (S == 'F' && height < 1.6){
     }
-    else if (S == 'F' && height >= 1.6 && hight < 1.75){
+    else if (S == 'F' && height >= 1.6 && height < 1.75){
     }
     else if (S == 'F' && height >= 1.75){
     }
